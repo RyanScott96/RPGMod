9d65cc5c8c82333ef2e7fa52ec2e70038f4ad0d0
Ryan L Scott <r.l.scott2014@gmail.com>
2021-07-17 13:11:31 -0400

    Add gradle build tools and src

---
+++ src/main/java/mod/arestlessknight/rpgmod/RPGMod.java
@@ -0,0 +1,31 @@
+package mod.arestlessknight.rpgmod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = RPGMod.MODID, name = RPGMod.NAME, version = RPGMod.VERSION)
+public class RPGMod
+{
+    public static final String MODID = "rpgmod";
+    public static final String NAME = "RPG MOD";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
